34d7a578e505aca57143fa51074a784c84a3b119
sachintha00 <sachintha0603@gmail.com>
2021-10-10 18:49:16 +0530

    updated

--- STACK/Stack.c
+++ STACK/Stack.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+struct Stack{
+    int arraStack[100];
+    int top;
+}st;
+
+int main()
+{
+    st.top = -1;
+    printf("Enter a stack size less than 100: ");
+}
